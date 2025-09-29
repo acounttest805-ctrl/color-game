b9cdf90e6b25291f856fd79b91d4806094c1a630
color master <acounttest805@gmail.com>
2025-09-29 19:02:30 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'color-game-cache-v4'; // 更新する際はここのバージョンを上げる
+const CACHE_NAME = 'color-game-cache-v4.1'; // 更新する際はここのバージョンを上げる
 const urlsToCache = [
   '/',
   '/index.html',
@@ -63,4 +63,5 @@ self.addEventListener('message', event => {
   }
 
 });
+
 
